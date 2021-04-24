c6d8bd280e634e6b4ca0ce402747b955667b1107
Anwesh43 <anweshthecool0@gmail.com>
2021-04-24 17:16:53 +0530

    adding BPNode class

--- index.ts
+++ index.ts
@@ -134,3 +134,45 @@ class Animator {
         }
     }
 }
+
+class BPNode {
+
+    next : BPNode 
+    prev : BPNode 
+    state : State = new State()
+
+    constructor(private i : number) {
+        this.addNeighbor()
+    }
+
+    addNeighbor() {
+        if (this.i < colors.length - 1) {
+            this.next = new BPNode(this.i + 1)
+            this.next.prev = this 
+        }
+    }
+
+    draw(context : CanvasRenderingContext2D) {
+        DrawingUtil.drawBPNode(context, this.i, this.state.scale)
+    }
+
+    update(cb : Function) {
+        this.state.update(cb)
+    }
+
+    startUpdating(cb : Function) {
+        this.state.startUpdating(cb)
+    }
+
+    getNext(dir : number, cb : Function) : BPNode {
+        var curr : BPNode = this.prev 
+        if (dir == 1) {
+            curr = this.next 
+        }
+        if (curr) {
+            return curr 
+        }
+        cb()
+        return this 
+    }
+}
