2abea6bf3157d695a5839a8fb5a30e29325227db
Anwesh43 <anweshthecool0@gmail.com>
2021-04-24 17:02:10 +0530

    adding DrawingUtil clasas

--- index.ts
+++ index.ts
@@ -1,6 +1,6 @@
 const w : number = window.innerWidth 
 const h : number = window.innerHeight 
-const parts : number = 3 
+const parts : number = 4
 const scGap : number = 0.02 / parts 
 const rFactor : number = 10.9 
 const barFactor : number = 4.9 
@@ -26,5 +26,37 @@ class ScaleUtil {
 
     static sinify(scale : number) : number {
         return Math.sin(scale * Math.PI)
+    }
+}
+
+class DrawingUtil {
+
+    static drawCircle(context : CanvasRenderingContext2D, x : number, y : number, r : number) {
+        context.beginPath()
+        context.arc(x, y, r, 0, 2 * Math.PI)
+        context.fill()
+    }
+
+    static drawBallPlace(context : CanvasRenderingContext2D, scale : number) {
+        const size : number = Math.min(w, h) / barFactor 
+        const r : number = Math.min(w, h) / rFactor 
+        const sf : number = ScaleUtil.sinify(scale)
+        context.save()
+        context.translate(w / 2, h / 2)
+        for (var j = 0; j < 2; j++) {
+            const sf1 : number = ScaleUtil.divideScale(sf, j, parts)
+            const sf2 : number = ScaleUtil.divideScale(sf, 2 + j, parts)
+            context.save()
+            context.scale(1 - 2 * j, 1)
+            context.fillRect(-w / 2, h / 2 - r, size * sf1, r)
+            DrawingUtil.drawCircle(context, -w / 2 + size - r, -h / 2 - r + (h / 2 - r) * sf2, r)
+            context.restore()
+        }
+        context.restore()
+    }
+
+    static drawBPNode(context : CanvasRenderingContext2D, i : number, scale : number) {
+        context.fillStyle = colors[i]
+        DrawingUtil.drawBallPlace(context, scale)
     }
 }
